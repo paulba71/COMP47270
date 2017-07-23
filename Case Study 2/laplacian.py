fe1456269c6a5a4c66d552ac9c9dade1e3691a93
Paul Barnes <paulba71@hotmail.com>
2017-07-23 16:53:48 +0100

    Using community networkx extension

--- Case Study 2/laplacian.py
+++ Case Study 2/laplacian.py
@@ -7,6 +7,7 @@
 import networkx as nx
 from networkx.algorithms import community
 
+
 # import the plotting functionality from matplotlib
 import matplotlib.pyplot as plt
 
@@ -22,6 +23,7 @@ import scipy as sp
 import random
 
 import platform
+import community
 
 
 if platform.system() != "Darwin":
@@ -213,32 +215,32 @@ def get_default_edges(graph):
     return graph
 
 
-def plot_graph(G, pos, fig_num):
+def plot_graph(graph, pos, fig_num):
 
     label = dict()
     label_pos = dict()
-    for i in range(G.number_of_nodes()):
+    for i in range(graph.number_of_nodes()):
         label[i] = i
         label_pos[i] = pos[i][0]+0.02, pos[i][1]+0.02
 
     fig = plt.figure(fig_num, figsize=(8, 8))
     fig.clf()
-    nx.draw_networkx_nodes(G, pos, node_size=40, hold=False)
-    nx.draw_networkx_edges(G, pos, hold=True)
-    nx.draw_networkx_labels(G, label_pos, label, font_size=10, hold=True)
+    nx.draw_networkx_nodes(graph, pos, node_size=40, hold=False)
+    nx.draw_networkx_edges(graph, pos, hold=True)
+    nx.draw_networkx_labels(graph, label_pos, label, font_size=10, hold=True)
     fig.show()
 
 
-def count_edge_cuts(G, w0, w1, w2):
+def count_edge_cuts(graph, w0, w1, w2):
     edge_cut_count = 0
     edge_uncut_count = 0
-    for edge in G.edges_iter():
+    for edge in graph.edges_iter():
         # This may be inefficient but I'll just check if both nodes are in 0, 1, or two
         if edge[0] in w0 and edge[1] in w0:
             edge_uncut_count += 1
         elif edge[0] in w1 and edge[1] in w1:
             edge_uncut_count += 1
-        elif edge[0] in w0 and edge[1] in w0:
+        elif edge[0] in w2 and edge[1] in w2:
             edge_uncut_count += 1
         else:
             edge_cut_count += 1
@@ -284,43 +286,87 @@ def cluster_nodes(graph, feat, pos, eigen_pos):
     nx.draw_networkx_nodes(graph, pos, node_size=40, hold=True, nodelist=W1, node_color='b')
 
 
-def placement():
+def analyse_graph (graph):
+    print("Graph dimensions:")
+    print("=================")
+    print(nx.info(graph))
+    max_degree = 0
+    min_degree = 999999
+    ave_degree = 0
+    counter = 0
+    for node in graph.nodes():
+        degree=graph.degree(node)
+        if degree > max_degree:
+            max_degree = degree
+        if min_degree > degree:
+            min_degree = degree
+        ave_degree += degree
+        counter += 1
+    ave_degree = ave_degree / counter
+
+    print ("====================")
+    print ("Max node degree: ", max_degree)
+    print ("Min node degree: ", min_degree)
+    print ("Ave node degree: ", ave_degree)
+    print ("====================")
+
+
+def get_community_partitions(graph):
+    partitions = community.best_partition(graph)
+    communities = [partitions.get(node) for node in graph.nodes()]
+    community_count = set(communities)
+    print("====================")
+    print("Community detected the following number of partitions: ", len(community_count))
+    print("====================")
+
+
+
+# def get_naive_partitions(graph):
+    # nodes=graph.
+
+def run_random_models():
     G = get_default_graph(False)
     pos = get_random_positions(G.number_of_nodes())
     # Need to rebuild the graph...
     A = nx.adjacency_matrix(G)  # will use the adjacency matrix later
     G = nx.Graph(A)
 
+    # Draw the nodes
     plot_graph(G, pos, 1)
 
     num_nodes = G.number_of_nodes()
 
     # Add some edges
     G = get_default_edges(G)
-
     # Need to rebuild the graph...
     A = nx.adjacency_matrix(G)
     G = nx.Graph(A)
 
+    # Print out the graph info...
+    analyse_graph(G)
+
+    # Draw the connected graph
     plot_graph(G,pos,2)
 
     # Get a random partition
     W0, W1, W2 = random_partition(G)
     count_edge_cuts(G, W0, W1, W2)
 
+    partitions = get_community_partitions(G)
+
     # Try using girvan_newman implementation in networkx
-    k = 3
-    G = nx.Graph(A)
-    comp = community.girvan_newman(G)
-    limited = itertools.takewhile(lambda c: len(c) <= k, comp)
-    for communities in limited:
-        print(tuple(sorted(c) for c in next(comp)))
+    #k = 3
+    #G = nx.Graph(A)
+    #comp = community.girvan_newman(G)
+    #limited = itertools.takewhile(lambda c: len(c) <= k, comp)
+    #for communities in limited:
+    #    print(tuple(sorted(c) for c in next(comp)))
 
     # Use the networkx implementation
-    communities = list(nx.k_clique_communities(G, num_nodes/3))
-    print("networkx W0: ", communities[0])
-    print("networkx W1: ", communities[1])
-    print("networkx W2: ", communities[2])
+    #communities = list(nx.k_clique_communities(G, num_nodes/3))
+    #print("networkx W0: ", communities[0])
+    #print("networkx W1: ", communities[1])
+    #print("networkx W2: ", communities[2])
 
     # Use the eigenvectors of the normalised Laplacian to calculate placement positions
     # for the nodes in the graph
@@ -359,5 +405,75 @@ def placement():
     input("Press Enter to Continue ...")
 
 
+def run_facebook_models():
+    G = load_graph("facebook")
+    pos = get_random_positions(G.number_of_nodes())
+    # Need to rebuild the graph...
+    A = nx.adjacency_matrix(G)  # will use the adjacency matrix later
+    G = nx.Graph(A)
+
+    # Draw the nodes
+    plot_graph(G, pos, 1)
+
+    num_nodes = G.number_of_nodes()
+
+    # Print out the graph info...
+    analyse_graph(G)
+
+    # Draw the connected graph
+    plot_graph(G, pos, 2)
+
+    # Get a random partition
+    W0, W1, W2 = random_partition(G)
+    count_edge_cuts(G, W0, W1, W2)
+
+    partitions = get_community_partitions(G)
+
+    # Try using girvan_newman implementation in networkx
+    # k = 3
+    # G = nx.Graph(A)
+    # comp = community.girvan_newman(G)
+    # limited = itertools.takewhile(lambda c: len(c) <= k, comp)
+    # for communities in limited:
+    #    print(tuple(sorted(c) for c in next(comp)))
+
+
+
+    # Use the eigenvectors of the normalised Laplacian to calculate placement positions
+    # for the nodes in the graph
+    # eigen_pos holds the positions
+    eigen_pos = dict()
+    deg = A.sum(0)
+    diags = np.array([0])
+    D = sp.sparse.spdiags(deg, diags, A.shape[0], A.shape[1])
+    Dinv = sp.sparse.spdiags(1 / deg, diags, A.shape[0], A.shape[1])
+    # Normalised laplacian
+    L = Dinv * (D - A)
+    E, V = sp.sparse.linalg.eigs(L, 3, None, 100.0, 'SM')
+    V = V.real
+
+    for i in range(num_nodes):
+        eigen_pos[i] = V[i, 1].real, V[i, 2].real
+
+    # for n,nbrsdict in G.adjacency_iter():
+    #     for nbr,eattr in nbrsdict.items():
+    #         if 'weight' in eattr:
+    #             print n,nbr,eattr['weight']
+
+    plot_graph(G, eigen_pos, 3)
+
+    # Now let's see if the eigenvectors are good for clustering
+    # Use kmeans to cluster the points in the vector V
+
+    features = np.column_stack((V[:, 1], V[:, 2]))
+    cluster_nodes(G, features, pos, eigen_pos)
+
+    # Finally, use the columns of A directly for clustering
+    cluster_nodes(G, A.todense(), pos, eigen_pos)
+
+    input("Press Enter to Continue ...")
+
+
 # Run an example.
-placement()
+# run_random_models()
+run_facebook_models()
